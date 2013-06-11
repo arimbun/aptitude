55cd941e45aa8a9ae1aa91afe7427436a7e91f43
arthur <anggiarto@gmail.com>
2013-06-12 00:26:48 +1000

    run collectstatic

--- aptitude/settings.py
+++ aptitude/settings.py
@@ -65,7 +65,7 @@ STATIC_ROOT = '/home/arthur/PycharmProjects/aptitude/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
 
 # Additional locations of static files
 STATICFILES_DIRS = (
