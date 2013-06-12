87c130761109f6208e3ea3d0907920e3b637ec15
arthur <anggiarto@gmail.com>
2013-06-12 21:54:40 +1000

    run collectstatic

--- aptitude/settings.py
+++ aptitude/settings.py
@@ -62,25 +62,20 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-# STATIC_ROOT = '/home/arthur/PycharmProjects/aptitude/static'
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT, '../')
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
-STATICFILES_DIRS = (
-    #os.path.join(PROJECT_ROOT, '../static/'),
-)
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-# STATICFILES_DIRS = (
-#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
-#     # Always use forward slashes, even on Windows.
-#     # Don't forget to use absolute paths, not relative paths.
-#     '/home/arthur/PycharmProjects/aptitude/apps/landing/static/',
-# )
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
 
 # List of finder classes that know how to find static files in
 # various locations.
