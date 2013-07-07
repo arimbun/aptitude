316bfcd5850177e040aff17200c00ed95021b617
Arthur Rimbun <anggiarto@gmail.com>
2013-07-07 17:47:47 +1000

    Revert "Changed requirements.txt"
    This reverts commit ecb3d5f3241be09cab8143917dc30146c43c449b.

--- aptitude/settings.py
+++ aptitude/settings.py
@@ -162,7 +162,6 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 # import dj_database_url
-#
 # DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
