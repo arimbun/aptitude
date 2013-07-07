28438df8fbec41ae470e3a63b0b461c20c064eb0
Arthur Rimbun <anggiarto@gmail.com>
2013-07-07 16:51:42 +1000

    Added overlays for fingerprints and US patent transcripts

---
+++ apps/landing/static/landing/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created with PyCharm.
+ * User: arthur
+ * Date: 7/07/13
+ * Time: 10:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function () {
+    $('a[rel]').overlay({
+        fixed: false
+    });
+});
