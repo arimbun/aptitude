b45e319fa7b860938fbafd9e1c71a3a26f930390
arthur <anggiarto@gmail.com>
2013-07-13 12:17:53 +1000

    Fix email sending on production

--- apps/booking_types/models.py
+++ apps/booking_types/models.py
@@ -10,3 +10,6 @@ class BookingTypes(models.Model):
         db_table = 'booking_types'
         verbose_name = _('BookingType')
         verbose_name_plural = _('BookingTypes')
+
+    def __unicode__(self):
+        return self.name + ' (AU$' + str(int(self.price)) + ')'
