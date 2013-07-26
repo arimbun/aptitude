292441afed51d8dcc5b17502abca36c2dc01d159
Arthur Rimbun <anggiarto@gmail.com>
2013-07-27 09:27:18 +1000

    Read confirmation email from a file

--- apps/landing/models.py
+++ apps/landing/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.core.mail import send_mail, BadHeaderError
 from django.db import models
 from django.http import HttpResponse
+import os
 
 
 class Landing(models.Model):
@@ -20,61 +21,17 @@ class Landing(models.Model):
         """
         title = 'Aptitude Assessment Booking Confirmation %s' % reference_number
         full_name = '%s %s' % (first_name, last_name)
-        message = """
-        Thank you for booking your aptitude assessment online. We look forward to helping you unveil your potentials. Below are the details for your booking.
 
-        Booking reference number: %s
-        Name: %s
-        Contact Number: %s
-        Email Address: %s
-        Country: %s
-        Postcode: %s
-        Booking type: %s
-        Data scanning appointment: %s (A consultant will contact you to advise of the appointment time)
-        Total Price: %s
-        Deposit Paid: %s
-        Total Owing: %s
-        Message to consultant: %s
-
-        Please find our booking terms & conditions below for your reference.
-
-        -------------------
-
-        Terms and Conditions:
-
-        Payment of balance
-        The outstanding balance is payable by the data scanning appointment time. We offer the following payment options:
-        1. Payment by direct bank transfer (preferred)
-            Account Name:               Aptitude World
-            BSB:                        032 257
-            Account number:             391306
-
-            NOTE: Payments must include the booking reference number and surname
-        2. Payment by cash at the data scanning appointment (please provide the exact amount)
-        3. Payment by credit card via our website.
-
-        Cancellation
-        Should you have to cancel your assessment, you must notify us at least 24 hour in advance. The deposit is not refundable but transferrable once only. Once biometric data is taken, no cancellation of service can be made.
-
-        Confidentiality
-        Your personal data is treated with utmost privacy and confidentiality. Once the aptitude report is generated, the biometric data is automatically deleted from the system. We will not use any personal data without your prior consent.
-
-        Disclaimer
-        The undersigned has understood the nature of the service, agreed to subscribe to the said service and its consultation. Aptitude World does not make any representation, statement or remark to the undersigned on what the customer will or will not do based on the aptitude analysis. The analysis does not predict what the future holds. Aptitude World shall take its commitment in providing its service to the customer and no discrepancies shall be arised by either party after the consultation.
-
-        -------------------
-
-        Kind Regards,
-        Aptitude World
-        Ph 1300 88 78 71
-        info@aptitudeworld.com.au
-        """ % (
+        confirmation_email_file = open(os.getcwd() + '/apps/landing/templates/landing/confirmation_email.html', 'r')
+        message = confirmation_email_file.read() % (
             reference_number, full_name, contact_number, email_to, country, postcode, booking_type,
             self.format_date(appointment_date), self.format_price(total_price), self.format_price(deposit_paid),
-            self.format_price(total_owing), message)
+            self.format_price(total_owing), message
+        )
 
         try:
-            send_mail(title, message, email_from, [email_to, 'info@aptitudeworld.com.au'], fail_silently=False)
+            # send_mail(title, message, email_from, [email_to, 'info@aptitudeworld.com.au'], fail_silently=False)
+            send_mail(title, message, email_from, [email_to], fail_silently=False)
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
 
