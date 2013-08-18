963e314c76e09417000beff080b4acb220eea2d9
arthur <anggiarto@gmail.com>
2013-08-18 17:37:11 +1000

    Fix Paypal payment going to sandbox instead of production

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -115,8 +115,6 @@ def confirm_booking(request):
     if request.method == 'POST':
         keywords, description, year = __init()
         title = COMPANY_NAME + ' - ' + 'Confirm Booking'
-        environment = os.environ['ENVIRONMENT']
-        # environment = 'production'
 
         personal_form = PersonalForm(request.POST)
         booking_form = BookingForm(request.POST)
@@ -183,7 +181,6 @@ def confirm_booking(request):
                               'total_amount': total_price_str,
                               'deposit_amount': deposit_paid_str,
                               'owing_amount': total_owing_str,
-                              'environment': environment,
                           }
             )
         else:
@@ -195,6 +192,7 @@ def confirm_booking(request):
 def book_success(request):
     if request.method == 'POST':
         environment = os.environ['ENVIRONMENT']
+        # environment = 'production'
         # get form data
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
